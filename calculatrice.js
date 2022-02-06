462e90ccdc84cd8ba9ce08c923f4c5b4f1090801
Edwin <edwin.d899@gmail.com>
2022-02-06 12:13:37 +0100

    function arythmétique

--- calculatrice.js
+++ calculatrice.js
@@ -1,14 +1,30 @@
-do {
-    var choix = prompt("Que souhaitez-vous faire? \n\n 1 - Addition \n 2 - Multiplication \n 3 - Soustraction \n 4 - Division" );
-}while(choix != 1 && choix != 2 && choix !=3 && choix != 4);
+//do {
+//    var choix = prompt("Que souhaitez-vous faire? \n\n 1 - Addition \n 2 - Multiplication \n 3 - Soustraction \n 4 - Division" );
+//}while(choix != 1 && choix != 2 && choix !=3 && choix != 4);
 
+//let premierNombre = Number();
+//let deuxiemeNombre = Number();
 
-do {
-    let premierNombre = Number(prompt('entre un nombre'))
-    let deuxiemeNombre= Number(prompt('entrez un deuxième nombre'))
+//do {
+//    premierNombre = Number(prompt('entre un nombre'));
+ //   deuxiemeNombre = Number(prompt('entrez un deuxième nombre'));
     
-}while(Number.isNaN) 
+//}while(Number.isNaN(premierNombre || deuxiemeNombre));
 
+//function addition(premierNombre, deuxiemeNombre) {
+  
+    
+//}
 
+let nombreUn    = 4;
+let nombreDeux  = 7;
 
- 
+function addition(nombreA, nombreB) {
+
+    let result = nombreA + nombreB;
+   // console.log(result);
+   return result;
+
+}
+
+console.log(addition(nombreUn, nombreDeux));
