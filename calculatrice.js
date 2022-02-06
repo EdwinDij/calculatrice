0dbf122ddb89c6914b0ddb49531022a165bc0b5e
Edwin <edwin.d899@gmail.com>
2022-02-06 11:11:08 +0100

    debut de l'étape 3 les fonction

---
+++ calculatrice.js
@@ -0,0 +1,14 @@
+do {
+    var choix = prompt("Que souhaitez-vous faire? \n\n 1 - Addition \n 2 - Multiplication \n 3 - Soustraction \n 4 - Division" );
+}while(choix != 1 && choix != 2 && choix !=3 && choix != 4);
+
+
+do {
+    let premierNombre = Number(prompt('entre un nombre'))
+    let deuxiemeNombre= Number(prompt('entrez un deuxième nombre'))
+    
+}while(Number.isNaN) 
+
+
+
+ 
