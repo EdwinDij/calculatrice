7bed53483b076ba55cf8ae2b098131d3fb9c5e0c
Edwin <edwin.d899@gmail.com>
2022-02-06 13:12:55 +0100

    fin de la caculatrice

--- calculatrice.js
+++ calculatrice.js
@@ -1,30 +1,64 @@
-//do {
-//    var choix = prompt("Que souhaitez-vous faire? \n\n 1 - Addition \n 2 - Multiplication \n 3 - Soustraction \n 4 - Division" );
-//}while(choix != 1 && choix != 2 && choix !=3 && choix != 4);
+//function arythmétique 
 
-//let premierNombre = Number();
-//let deuxiemeNombre = Number();
 
-//do {
-//    premierNombre = Number(prompt('entre un nombre'));
- //   deuxiemeNombre = Number(prompt('entrez un deuxième nombre'));
-    
-//}while(Number.isNaN(premierNombre || deuxiemeNombre));
-
-//function addition(premierNombre, deuxiemeNombre) {
-  
-    
-//}
-
-let nombreUn    = 4;
-let nombreDeux  = 7;
 
 function addition(nombreA, nombreB) {
-
-    let result = nombreA + nombreB;
-   // console.log(result);
-   return result;
+    return nombreA + nombreB;
 
 }
 
-console.log(addition(nombreUn, nombreDeux));
+function multiplication(nombreA, nombreB) {
+    return nombreA * nombreB;
+
+}
+function soustraction(nombreA, nombreB) {
+    return nombreA - nombreB;
+
+}
+
+function division(nombreA, nombreB) {
+    if (nombreB == 0) {
+        throw new Error("interdit");
+    }
+    return nombreA / nombreB;
+
+}
+
+do {
+    var choix = Number(prompt("Que souhaitez-vous faire? \n\n 1 - Addition \n 2 - Multiplication \n 3 - Soustraction \n 4 - Division"));
+} while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
+
+
+do {
+    var premierNombre = Number(prompt('entre un nombre:'));
+    var deuxiemeNombre = Number(prompt('entrez un deuxième nombre:'));
+
+} while (isNaN(premierNombre) || isNaN(deuxiemeNombre));
+
+try {
+    switch (choix) {
+        case 1:
+            var resultat = addition(premierNombre, deuxiemeNombre);
+            break;
+
+        case 2:
+            var resultat = multiplication(premierNombre, deuxiemeNombre);
+            break;
+
+        case 3:
+            var resultat = soustraction(premierNombre, deuxiemeNombre);
+            break;
+
+        case 4:
+            var resultat = division(premierNombre, deuxiemeNombre);
+            break;
+        default:
+            throw new
+                Error("une erreur s'est glissé")
+    }
+    alert("voici le resultat : " + resultat);
+}
+catch (error) {
+    alert(error);
+}
+
